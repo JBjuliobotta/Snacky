8fabbe5473af70a99d52239da08cf1f455329d41
centu19 <estebancenturion4@hotmail.com>
2024-04-25 21:39:44 -0300

    se maqueto la pagina de contacto

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import { Form, Button } from "react-bootstrap";
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Contactos</h1>
+        <div className="container py-3 my-3 ">
+            <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email </Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Deja tus comentarios</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+        <Button variant="primary" type="submit">
+        Enviar
+      </Button>
+      </Form.Group>
+    </Form>
+             
+                
         </div>
     );
 };
