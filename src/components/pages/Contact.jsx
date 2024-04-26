966db76d6ecc14def3a3564fc12ac71cc1120449
centu19 <estebancenturion4@hotmail.com>
2024-04-26 14:19:44 -0300

    se hicieron las validaciones con json y dbjson, por fin me salio

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,8 +3,13 @@ import Iframe from 'react-iframe'
 import * as Yup from "yup";
 import { useFormik } from "formik";
 import clsx from "clsx";
+import Swal from 'sweetalert2' 
+import { json } from "react-router-dom";
+
 
 const Contact = () => {
+  const API = import.meta.env.VITE_API;
+  
   const ComentariosSchema = Yup.object().shape({
     nombre: Yup.string()
       .min(4, "min 4 caract.")
@@ -33,6 +38,34 @@ const Contact = () => {
     validateOnChange: true,
     onSubmit: (values) => {
       console.log("Values de Formik-->", values);
+      Swal.fire({
+        title: "¿Enviar tus comentario?",
+        
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Enviar"
+      }) .then(async (result) => {
+        if (result.isConfirmed) {
+          try {
+            const response=await fetch(`${API}/comentarios`,{method:"POST", 
+          headers:{"content-type":"aplication/json",},
+        body: JSON.stringify (values)});
+            if (response.status === 201) {
+              formik.resetForm();
+              Swal.fire({
+                title: "¡Exito!",
+                text: "Se enviaron tus comentarios",
+                icon: "success",
+              });
+             
+            }
+          } catch (error) {
+            console.log("ERROR-->", error);
+          }         
+        }
+      });
     },
   });
     return (
@@ -90,33 +123,33 @@ const Contact = () => {
          }
        )}>
       <option className="mb-3" >Eleji una opcion</option>
-      <option value="1">Buenos Aires</option>
-                                <option value="2">Capital Federal</option>
-                                <option value="3">Catamarca</option>
-                                <option value="4">Chaco</option>
-                                <option value="5">Chubut</option>
-                                <option value="6">Cordoba</option>
-                                <option value="7">Corrientes</option>
-                                <option value="8">Entre Ríos</option>
-                                <option value="9">Formosa</option>
-                                <option value="10">Jujuy</option>
-                                <option value="11">La Pampa</option>
-                                <option value="12">La Rioja</option>
-                                <option value="13">Mendoza</option>
-                                <option value="14">Misiones</option>
-                                <option value="15">Neuquén</option>
-                                <option value="16">Rio Negro</option>
-                                <option value="17">Salta</option>
-                                <option value="18">San Juan</option>
-                                <option value="19">San Luis</option>
-                                <option value="20">Santa Cruz</option>
-                                <option value="21">Santa Fe</option>
-                                <option value="22">Santiago del Estero</option>
-                                <option value="23">Tierra del Fuego</option>
-                                <option value="24">Tucumán</option>
+      <option value="Buenos Aires">Buenos Aires</option>
+                                <option value="Capital Federal">Capital Federal</option>
+                                <option value="Catamarca">Catamarca</option>
+                                <option value="Chaco">Chaco</option>
+                                <option value="Chubut">Chubut</option>
+                                <option value="Cordoba">Cordoba</option>
+                                <option value="Corrientes">Corrientes</option>
+                                <option value="Entre Rios">Entre Ríos</option>
+                                <option value="Formosa">Formosa</option>
+                                <option value="Jujuy">Jujuy</option>
+                                <option value="La Pampa">La Pampa</option>
+                                <option value="La Rioja">La Rioja</option>
+                                <option value="Mendoza">Mendoza</option>
+                                <option value="Misiones">Misiones</option>
+                                <option value="Neuquén">Neuquén</option>
+                                <option value="Rio Negro">Rio Negro</option>
+                                <option value="Salta">Salta</option>
+                                <option value="San Juan">San Juan</option>
+                                <option value="San Luis">San Luis</option>
+                                <option value="Santa Cruz">Santa Cruz</option>
+                                <option value="Santa Fe">Santa Fe</option>
+                                <option value="Santiago del Estero">Santiago del Estero</option>
+                                <option value="Tierra del Fuego">Tierra del Fuego</option>
+                                <option value="Tucuman">Tucumán</option>
     </Form.Select>
     {formik.touched.opcionlugar && formik.errors.opcionlugar && (
-            <div className="mt-2 text-danger fw-bolder">
+            <div className="mt-2 text-warning fw-bolder">
               <span role="alert">{formik.errors.opcionlugar}</span>
             </div>
           )}
@@ -139,7 +172,7 @@ const Contact = () => {
          )}
        />
        {formik.touched.comentarios && formik.errors.comentarios && (
-         <div className="mt-2 text-danger fw-bolder">
+         <div className="mt-2 text-warning fw-bolder">
            <span role="alert">{formik.errors.comentarios}</span>
          </div>
        )}
