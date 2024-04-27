1d15cd6f6d6b49517564b51016279a4c295e5328
Facu Moyano <facundo4349674@gmail.com>
2024-04-27 17:56:39 -0300

    Se settearon imagenes para el slider del home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,42 @@
-import React, { useContext } from 'react';
-import UserContext from '../context/UserContext';
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
+import Carousel from "react-bootstrap/Carousel";
+import gJ from "../../assets/Grupo_Jovenes.png"
 
 const Home = () => {
-    const {currentUser}=useContext(UserContext);
+  const { currentUser } = useContext(UserContext);
 
-    console.log("currentuser", currentUser);
+  console.log("currentuser", currentUser);
 
-    return (
-        <div>
-            <h1>Inicio</h1>
-        </div>
-    );
+  return (
+    <>
+      <Carousel>
+        <Carousel.Item interval={1000}>
+            <img src={gJ} alt="" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+        <img src={snacky} alt="" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img src={snacky} alt="" />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
 };
 
 export default Home;
