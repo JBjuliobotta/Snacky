47e1bb222ca69cc3ae801e8e8b28b819d328b8fe
Axel <axelyoelontiveros@gmail.com>
2024-04-27 18:58:09 -0300

    se agrego la pagina about us

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -1,11 +1,174 @@
 import React from 'react';
-
-const AboutUs = () => {
+import '../css/aboutus.css'
+const Aboutus = () => {
     return (
-        <div>
-            <h1>Sobre nosotros</h1>
+        <section>
+        <div className="wrapper">
+
+        <div className="title">
+            <h4>Integrantes</h4>
         </div>
+
+        <div className="card_Container">
+
+            <div className="card">
+
+                <div className="imbBx">
+                    <img src="src/assets/Esteban Centurión.jpeg" alt=""/>
+                </div>
+
+                <div className="content">
+                    <div className="contentBx">
+                        <h3>Esteban Centurión<br/><span>Programador</span></h3>
+                    </div>
+                    <ul className="sci" style={{
+          marginRight:"11%"}}>
+                        <li>
+                            <a href="#"><i className="fa-brands fa-instagram"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i className="fa-brands fa-github"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i className="fa-brands fa-linkedin-in"></i></a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div className="card">
+
+<div className="imbBx">
+    <img src="src/assets/Julio.jpeg" alt=""/>
+</div>
+
+<div className="content">
+    <div className="contentBx">
+        <h3>Julio Rafael Bottanpm  <br/><span>Programador</span></h3>
+    </div>
+    <ul className="sci"style={{
+          marginRight:"11%"}}>
+        <li >
+            <a href="#"><i className="fa-brands fa-instagram"></i></a>
+        </li>
+        <li >
+            <a href="#"><i className="fa-brands fa-github"></i></a>
+        </li>
+        <li >
+            <a href="#"><i className="fa-brands fa-linkedin-in"></i></a>
+        </li>
+    </ul>
+</div>
+
+</div>
+
+            <div className="card">
+
+                <div className="imbBx">
+                    <img src="src/assets/Axel Yoel Ontiveros.jpg" alt=""/>
+                </div>
+
+                <div className="content">
+                    <div className="contentBx">
+                        <h3>Axel Yoel Ontiveros <br/><span>Programador</span></h3>
+                    </div>
+                    <ul className="sci"style={{
+          marginRight:"11%"}}>
+                        <li>
+                            <a href="#"><i className="fa-brands fa-instagram"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i className="fa-brands fa-github"></i></a>
+                        </li>
+                        <li >
+                            <a href="#"><i className="fa-brands fa-linkedin-in"></i></a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div className="card">
+
+                <div className="imbBx">
+                    <img src="src/assets/Facundo Moyano.jpeg" alt=""/>
+                </div>
+
+                <div className="content">
+                    <div className="contentBx">
+                        <h3>Facundo Moyano <br/><span>Programador</span></h3>
+                    </div>
+                    <ul className="sci"style={{
+          marginRight:"11%"}}>
+                        <li >
+                            <a href="#"><i className="fa-brands fa-instagram"></i></a>
+                        </li>
+                        <li >
+                            <a href="#"><i className="fa-brands fa-github"></i></a>
+                        </li>
+                        <li >
+                            <a href="#"><i className="fa-brands fa-linkedin-in"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                
+            </div>
+            <div className="card">
+
+        <div className="imbBx">
+            <img src="src/assets/Fernando Abregu.jpeg" alt=""/>
+        </div>
+
+        <div className="content">
+            <div className="contentBx">
+                <h3>Fernando Abregu<br/><span>Programador</span></h3>
+            </div>
+            <ul className="sci"style={{
+          marginRight:"11%"}}>
+            <li>
+            <a href="#"><i className="fa-brands fa-instagram"></i></a>
+        </li>
+        <li>
+            <a href="#"><i className="fa-brands fa-github"></i></a>
+        </li>
+        <li>
+            <a href="#"><i className="fa-brands fa-linkedin-in"></i></a>
+        </li>
+    </ul>
+</div>
+
+</div>
+<div className="card">
+
+<div className="imbBx">
+    <img src="src/assets/Jose Maximiliano Leal.jpeg" alt=""/>
+</div>
+
+<div className="content">
+    <div className="contentBx">
+        <h3>Jose Maximiliano Leal <br/><span>Programador</span></h3>
+    </div>
+    <ul className="sci"style={{
+          marginRight:"11%"}}>
+        <li >
+            <a href="#"><i className="fa-brands fa-instagram"></i></a>
+        </li>
+        <li >
+            <a href="#"><i className="fa-brands fa-github"></i></a>
+        </li>
+        <li >
+            <a href="#"><i className="fa-brands fa-linkedin-in"></i></a>
+        </li>
+    </ul>
+</div>
+
+</div>
+        </div>
+
+    </div>
+    </section>
     );
 };
 
-export default AboutUs;
+export default Aboutus;
